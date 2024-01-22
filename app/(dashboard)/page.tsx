36756d4e0fcc03fff6005407ebb18a8c2e071ed4
vishh2dev <vishh2dev@gmail.com>
2024-01-22 23:12:14 +0530

    feat: navbar

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,12 @@
+
+import React, { ReactNode } from "react";
+
+function Home({ children }: { children: ReactNode }) {
+  return (
+    <div >
+     hello
+    </div>
+  );
+}
+
+export default Home;
