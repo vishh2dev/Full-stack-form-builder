f79313dee81fc3778552ed5b3c50d242bec677f3
vishh2dev <vishh2dev@gmail.com>
2024-02-11 21:48:57 +0530

    feat:designercontext

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,7 @@ import "./globals.css";
 import { ClerkProvider } from '@clerk/nextjs'
 import { ThemeProvider } from "@/components/providers/ThemeProvider";
 import { Toaster } from "@/components/ui/toaster";
-
-
+import DesignerContextProvider from "@/components/context/DesignerContext";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -22,19 +21,21 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body className={inter.className}>
-      <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-            <Toaster/>
-          </ThemeProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <DesignerContextProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
+              {children}
+              <Toaster />
+            </ThemeProvider>
+          </DesignerContextProvider>
         </body>
-    </html>
+      </html>
     </ClerkProvider>
   );
 }
